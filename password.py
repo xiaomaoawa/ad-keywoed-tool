601a82ff6411ea85b9d820da29039349431a9df4
陈信通 <chenxintong@example.com>
2026-04-30 13:28:00 +0800

    Disable activation verification

--- password.py
+++ password.py
@@ -1,4 +1,4 @@
 from run import generate_activation_code
-machine_code = "A3C09C019393AFB2"  # 如：A1B2C3D4E5F67890
+machine_code = "13083247AF7B8FDE"  # 如：A1B2C3D4E5F67890
 activation_code = generate_activation_code(machine_code)
 print(f"激活码: {activation_code}")
